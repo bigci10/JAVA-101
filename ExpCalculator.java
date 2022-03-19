399857acddeb48c5cbd1aa4ba9473137a403f595
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-03-19 09:00:23 -0400

    Create ExpCalculator.java

---
+++ ExpCalculator.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+import static java.lang.System.*;
+
+public class Solutions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        long result = 1;
+
+        for (;r != 0;r--){
+            result *= n;
+        }
+        System.out.println(result);
+
+
+    }
+}
