1beca56285a403514282344d733c7674ac390bfb
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-03-19 09:51:48 -0400

    Create 22-Harmonicnum.java

---
+++ 22-Harmonicnum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+import static java.lang.System.*;
+
+public class Solutions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+        int number= sc.nextInt();
+        double result = 0;
+
+        for (double i = 1; i <= number; i++){
+            result += (1/i);}
+        System.out.println(result);
+
+    }
+}
