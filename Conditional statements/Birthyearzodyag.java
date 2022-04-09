f7b68099405722b379507faccb8a33faf5530260
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-04-09 08:14:47 -0400

    Create Birthyearzodyag.java

---
+++ Conditional statements/Birthyearzodyag.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+
+public class birthyearzodyag {
+
+    public static void main(String[] args) {
+        int birthyear;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter birth year :");
+        birthyear = input.nextInt();
+
+        switch (birthyear % 12){
+            case 0:
+                System.out.println("Monkey");
+                break;
+            case 1:
+                System.out.println("Cock");
+                break;
+            case 2:
+                System.out.println("Dog");
+                break;
+            case 3:
+                System.out.println("Pig");
+                break;
+            case 4:
+                System.out.println("Mouse");
+                break;
+            case 5:
+                System.out.println("Bull");
+                break;
+            case 6:
+                System.out.println("Tiger");
+                break;
+            case 7:
+                System.out.println("Rabbit");
+                break;
+            case 8:
+                System.out.println("Dragon");
+                break;
+            case 9:
+                System.out.println("Snake");
+                break;
+            case 10:
+                System.out.println("Horse");
+                break;
+            case 11:
+                System.out.println("Sheep");
+                break;
+        }
+
+    }
+}
