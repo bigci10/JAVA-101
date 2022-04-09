49d268c409068721b3cae2b5a90eacf8bc7dd416
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-04-09 08:26:29 -0400

    Create ExpSolution.java

---
+++ Loops/ExpSolution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+//We write a program that prints powers of 4 and 5 up to the number entered with Java loops.
+
+
+public class Solutions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Please enter the last number: ");
+
+        int five = 1;
+        for (int i = 1;i < n; i*=4){
+            System.out.println(i);
+            System.out.println(five);
+            five*=5;
+
+
+        }
+    }
+}
