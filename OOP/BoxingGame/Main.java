21386b05036f3ba087728f83ee706a7f8b2df6a3
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-06-15 20:09:41 +0300

    Add files via upload

---
+++ OOP/BoxingGame/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Fighter fighter1 = new Fighter("Bilal İğci",100,90);
+        Fighter fighter2 = new Fighter("Fatih Yıldız",75,75);
+
+        Match match1 = new Match("World Heavy Championship",15,fighter1,fighter2);
+
+        match1.playMatch();
+
+
+
+    }
+
+
+
+}
---
+++ OOP/BoxingGame/Match.java
@@ -0,0 +1,68 @@
+public class Match {
+
+    String title;
+    int round ;
+    Fighter fighter1;
+    Fighter fighter2;
+    private String winner ;
+
+    Match(String tittle,int round,Fighter fighter1,Fighter fighter2){
+        this.title = tittle;
+        this.round = round;
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+    }
+
+    public void playMatch(){
+        int i = 0;
+        while (i < this.round){
+            if (fighter2.getHealth() > 0){
+                int upgrde = fighter2.getHealth()-fighter1.Punch();
+                fighter2.setHealth(upgrde);
+
+                if(fighter1.getHealth() > 0){
+                    int upgrde2 = fighter1.getHealth()-fighter2.Punch();
+                    fighter1.setHealth(upgrde2);
+                }
+                else{
+                    this.winner = "The Winner is "+fighter2.getName();
+                    break;
+                }
+
+                i++;
+            }
+            else{
+                this.winner = "The Winner is " + fighter1.getName();
+                break;
+            }
+
+
+
+
+    }
+
+
+
+
+        System.out.println(winner);
+
+    }
+    //Title ne maçı  name
+    //fighter1
+    // fighter 2
+
+    //fighter1 points
+    //fighter2 points
+
+
+    //metotlar
+
+    //playMatch(fighter1 ve 2){
+
+//}
+
+    //stopmaç (maç durduruldu)
+
+    //total rounds(toplam round)
+
+}
