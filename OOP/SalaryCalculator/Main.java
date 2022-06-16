bd0d4d6e466653e5fcf1bafd227d22d97b93c1ae
Bilal Igci <70436168+bigci10@users.noreply.github.com>
2022-06-16 13:33:23 +0300

    Create Main.java

---
+++ OOP/SalaryCalculator/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  
+  public static void main(String[] args) {
+    
+    Worker work1 = new Worker("Muhammed Bilal İğci",1001,45,2020);
+    
+    System.out.println(work1.toString());
+  
+  
+  }
+
+
+}
